0181c95988de513ad9938f0274dfc66dac3e4f86
Abu-muaz <plshireseid@gmail.com>
2025-09-06 21:51:16 +0300

    fix the path

--- Netflix_Clone/vite.config.js
+++ Netflix_Clone/vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
-  server:{
-    port:3000,
-  }
-})
+  base: "/NETFLIX-Clone/", // Add this line
+});
