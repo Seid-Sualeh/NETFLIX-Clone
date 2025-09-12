2b1d681821647d7d23eb9a755f19fb4ed07da437
Abu-muaz <plshireseid@gmail.com>
2025-09-13 02:57:17 +0300

    package add

--- Netflix_Clone/vite.config.js
+++ Netflix_Clone/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/NETFLIX-Clone',
   plugins: [react()],
-  base: "/NETFLIX-Clone/", // Add this line
+
 });
